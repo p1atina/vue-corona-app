d0dfb35d0f8a3d5f06952e3382b3f58cd09db583
zeroday <36220595+p1atina@users.noreply.github.com>
2023-09-12 17:12:42 +0900

    Create koraia.py

---
+++ koraia.py
@@ -0,0 +1,19 @@
+import subprocess
+
+process = subprocess.Popen("./sp_firedetect", stdout=subprocess.PIPE, shell=True)
+
+print("||||First")
+
+while True:
+    output = process.stdout.readline()
+    print("||||First 0000")
+    if output == b'' and process.poll() is not None:
+        print("||||F11111")
+        break
+    if b'CConsoleServer::initialize() [127.0.0.1:35000] initialized' in output:
+        process.kill()
+        print("||||F22222")
+        break
+    if output:
+        print(output.strip())
+        print("||||F33333")
