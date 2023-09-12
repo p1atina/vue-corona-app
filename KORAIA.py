4a7bbb4d69921459b9a5514054e27f08dc18cdcc
zeroday <36220595+p1atina@users.noreply.github.com>
2023-09-12 16:55:39 +0900

    Update KORAIA.py

--- KORAIA.py
+++ KORAIA.py
@@ -2,13 +2,18 @@ import subprocess
 
 process = subprocess.Popen("./sp_firedetect", stdout=subprocess.PIPE, shell=True)
 
+print("실행타나요?")
+
 while True:
     output = process.stdout.readline()
-    print(output)
+    print("실행타나요? 111")
     if output == b'' and process.poll() is not None:
+        print("실행타나요?첫번째")
         break
     if b'mpeg4' in output:
         process.terminate()
+        print("실행타나요? 두번째")
         break
     if output:
         print(output.strip())
+        print("실행타나요? 세번째")
